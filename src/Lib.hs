142de26b67ae7ddb25f4bbaad72c4661942d6e4b
joranvar <bart.post@gmail.com>
2016-10-27 15:15:47 +0200

    Describe the possible operations

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,5 +1,9 @@
 -- | Countdown -- Inspired by https://www.futurelearn.com/courses/functional-programming-haskell/1/steps/120287
 module Lib
   (
-    -- * Exported functions
+    -- * Domain types
+    Operation(..)
   ) where
+
+-- | The possible operations to reach the solution
+data Operation = Plus | Minus | Times | DivideBy
