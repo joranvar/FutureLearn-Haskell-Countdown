3efbd7fbbb131fe384d8fc9c505a84b25d86ccbc
joranvar <bart.post@gmail.com>
2016-10-27 15:21:41 +0200

    Describe the solution

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,7 +3,15 @@ module Lib
   (
     -- * Domain types
     Operation(..)
+  , Solution
+  , Value(..)
   ) where
 
 -- | The possible operations to reach the solution
 data Operation = Plus | Minus | Times | DivideBy
+
+-- | The stack of operations and numbers that will lead to a solution
+type Solution = [Value]
+
+-- | A value that can be put on the stack for execution
+data Value = Op Operation | Val Int
